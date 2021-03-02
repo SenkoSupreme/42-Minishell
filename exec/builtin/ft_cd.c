1271fcbd18ac8f4d9063058862730b8dfe3ee7d0
Saad El fadil <sel-fadi@e2r7p2.1337.ma>
2021-03-02 14:59:19 +0100

    ft_cd updated...

--- exec/builtin/ft_cd.c
+++ exec/builtin/ft_cd.c
@@ -1,11 +1,24 @@
 #include "../../minishell.h"
 
+int norme_hack(char **argv, char *cur)
+{
+    char pwdold[1000];
+    char *oldcur;
+    if (chdir(argv[1]) == 0)
+    {
+        oldcur = getcwd(pwdold, 200);
+        add_element("PWD", oldcur);
+        add_element("OLDPWD", cur);
+    }
+    else
+        senko_print("cd: ", "no such file or directory: ", argv[1], "\n");
+    return (1);
+}
+
 void ft_cd(char **argv)
 {
     char *cur;
-    char *oldcur;
     char pwdcur[1000];
-    char pwdold[1000];
     char *str;
 
     cur = getcwd(pwdcur, 200);
@@ -16,17 +29,7 @@ void ft_cd(char **argv)
         chdir(argv[0]);
     }
     else if (argv[0] && argv[1] && !argv[2])
-    {
-        if (chdir(argv[1]) == 0)
-        {
-            oldcur = getcwd(pwdold, 200);
-            add_element("PWD", oldcur);
-            add_element("OLDPWD", cur);
-            printf("Directory Changed...\n");
-        }
-        else
-            senko_print("cd: ", "no such file or directory: ", argv[1], "\n");
-    }
+        norme_hack(argv, cur);
     else if (argv[3])
         ft_putstr_fd("cd: too many arguments\n", 2);
     else if (argv[2] && !argv[3])
