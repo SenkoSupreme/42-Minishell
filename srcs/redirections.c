17abb87a114f628917e89cd4d82c1a14b8cf4904
nekosenko <brija.main@gmail.com>
2021-03-07 11:55:54 +0100

    flashed a sterror

--- srcs/redirections.c
+++ srcs/redirections.c
@@ -6,7 +6,7 @@
 /*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 15:28:24 by mbrija            #+#    #+#             */
-/*   Updated: 2021/03/07 11:38:58 by mbrija           ###   ########.fr       */
+/*   Updated: 2021/03/07 11:54:12 by mbrija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int				open_file(char *file, int flag, char *type)
 	if (fd == -1)
 	{
 		senko_print("SSHELL: ", file, ": ", strerror(errno));
-		ft_putstr_fd("n", 2);
+		ft_putstr_fd("\n", 2);
 		g_minishell.ret = 1;
 		g_minishell.status = 0;
 	}
